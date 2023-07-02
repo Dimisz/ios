82ab26115ee491d71cef2dd75ee3015a2c3546c3
Vladimir Solovyov <leilyngale@icloud.com>
2023-07-02 19:14:06 +0800

    Chapter 1

--- swiftApprentice/chapter01.playground/Contents.swift
+++ swiftApprentice/chapter01.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-(2.0).squareRoot()
+let myAge: Int = 35
+var averageAge: Double = Double(myAge)
+print(averageAge)
+averageAge = (Double(myAge) + 30) / 2
+print(averageAge)
+
+let testNumber: Int = 42
+let evenOdd: Int = testNumber % 2
+print(evenOdd)
+
